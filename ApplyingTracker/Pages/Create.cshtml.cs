68f3894190b37794e0b8f15c87406c297b0daf54
Sebastian <mrseb10@gmail.com>
2024-08-27 11:39:22 +0200

    Creating application

---
+++ ApplyingTracker/Pages/Create.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Services;
+
+namespace ApplyingTracker.Pages;
+
+public class CreateModel : PageModel
+{
+    private readonly IApplicationsService _applicationsService;
+    [BindProperty] public Application Application { get; set; } = new();
+
+    public CreateModel(IApplicationsService applicationsService)
+    {
+        _applicationsService = applicationsService;
+    }
+    
+    public void OnGet()
+    {
+        
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid) return Page();
+
+        await _applicationsService.AddApplication(Application);
+        return Redirect("/");
+    }
+}
